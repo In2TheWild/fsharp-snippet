cc4e1c6ae3b17d5ee1b3a6ffb52719eb02bdb41b
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-10-31 12:33:17 +0700

    test seq

---
+++ src/lang/seq.fsx
@@ -0,0 +1,31 @@
+let failures = ref []
+
+let reportFailure (s : string) =
+    stderr.Write" NO: "
+    stderr.WriteLine s
+    failures := !failures @ [s]
+
+let check s e r =
+  if r = e then  stdout.WriteLine (s + ": YES")
+  else (stdout.WriteLine ("\n***** " + s + ": FAIL\n"); reportFailure s)
+
+check "rwfsjkla"
+     (let mutable results = []
+      let ys =
+          seq {
+              try
+                  try
+                      failwith "foo"
+                  finally
+                      results <- 1::results
+                      failwith "bar"
+              finally
+                  results <- 2::results
+          }
+      try
+          printfn "%A" ys
+          for _ in ys do ()
+      with
+          Failure "bar" -> results <- 3::results
+      results)
+      [3;2;1]
