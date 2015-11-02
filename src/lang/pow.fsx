5e3b2aaefbd43ed07acc24ebc7da2013fe6eafe5
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-11-02 13:27:49 +0700

    fix pow

--- src/lang/pow.fsx
+++ src/lang/pow.fsx
@@ -1,9 +1,14 @@
 
-type T(x: float) =
+type N(x: float) =
     member val Value = x with get
-    static member Pow(g: T, e: float) = e ** g.Value
+    static member Pow(a:N, b:N) = N(a.Value ** b.Value)
 
-let t = new T(1.)
-let c = t ** 3.
+type S(x: string) =
+    member val Value = x with get
+    static member Pow(a:S, b:S) = S(a.Value + b.Value + a.Value)
 
-printfn "%A" c
+let n = N(2.) ** N(2.)
+let s = S("ABC") ** S("DEF")
+
+printfn "%A" n.Value    // 4.0
+printfn "%A" s.Value    // "ABCDEFABC"
